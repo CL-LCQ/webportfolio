7d8e876dc0ca14238e19644c5ff88b85e492ced0
Charles Leclercq <2dcharles@gmail.com>
2024-09-16 19:37:27 +0100

    updated overlay mechanism

--- scripts/domEvents.js
+++ scripts/domEvents.js
@@ -4,7 +4,9 @@ export function addOverlayToggleListener() {
 
   if (overlayToggle && overlay) {
     overlayToggle.addEventListener('click', () => {
-      overlay.classList.toggle('transparent');
+      overlay.classList.toggle('minimized');
+      // overlay.classList.toggle('overlay-collapsed'); // Add or remove class based on current state
+
     });
   } else {
     console.error('Element(s) not found: overlay-toggle or info-overlay');
