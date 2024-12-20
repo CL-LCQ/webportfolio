13b25bfc3ef7d1d29a3f607bdb6468d55d4f3430
Charles Leclercq <2dcharles@gmail.com>
2024-12-20 14:10:16 +0000

    Remove cache files from Git tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { supabaseClient } from './supabaseClient';
 import HeaderContent from './components/Header/Header'; 
 import './App.css';
@@ -11,23 +11,17 @@ function App() {
 
   useEffect(() => {
     const fetchProjects = async () => {
-      try {
-        const { data, error } = await supabaseClient.from('work_v2').select('*');
-        if (error) {
-          console.error('Error fetching projects:', error);
-          setLoading(false);
-          return;
-        }
-
-        const filteredData = data.filter((project) => project.enabled !== false); // Only enabled projects
-        const sortedData = filteredData.sort((a, b) => a.order - b.order); // Sort projects by "order" field
-
-        setProjects(sortedData);
-      } catch (error) {
-        console.error('Unexpected error:', error);
-      } finally {
-        setLoading(false); 
+      const { data, error } = await supabaseClient.from('work_v2').select('*');
+      if (error) {
+        console.error('Error fetching projects:', error);
+        setLoading(false);
+        return;
       }
+
+      const filteredData = data.filter((project) => project.enabled !== false);
+      const sortedData = filteredData.sort((a, b) => a.order - b.order);
+      setProjects(sortedData);
+      setLoading(false); 
     };
     fetchProjects();
   }, []);
@@ -38,13 +32,9 @@ function App() {
         <header className="app-header">
           <HeaderContent />
         </header>
-
         <main className="app-main">
           <Routes>
-            {/* Route for the home page */}
             <Route path="/" element={<HomePage projects={projects} loading={loading} />} />
-            
-            {/* Route for the dynamic project page /project/:id */}
             <Route path="/project/:projectId" element={<HomePage projects={projects} loading={loading} />} />
           </Routes>
         </main>
